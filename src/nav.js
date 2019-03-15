28ee3599d27e1ae7de4971f3f8767500ed3bbce5
Mark Yapp <markwyapp@gmail.com>
2019-03-14 22:40:11 -0600

    styling

--- src/nav.js
+++ src/nav.js
@@ -2,23 +2,44 @@ import React from 'react';
 
 import './nav.css';
 
-export default function Nav() {
-  return (
-    <div>
-      <header>
-        <h1 className="header">Mark Yapp</h1>
-      </header>
-      <nav>
-        <a href="#profile">
-          <button>About Me</button>
-        </a>
-        <a href="#projects">
-          <button>Projects</button>
-        </a>
-        <a href="#contact">
-          <button>Contact</button>
-        </a>
-      </nav>
-    </div>
-  );
+export default class Nav extends React.Component {
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    const navbar = document.getElementById('navbar');
+    const main = document.getElementById('main');
+
+    // let sticky = navbar.offsetTop;
+    const headerHeight = 120;
+    if (window.pageYOffset >= headerHeight) {
+      navbar.classList.add('sticky');
+      main.classList.add('extra-padding');
+    } else {
+      navbar.classList.remove('sticky');
+      main.classList.remove('extra-padding');
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1>Mark Yapp</h1>
+        </header>
+        <nav id="navbar">
+          <a href="#profile">
+            <button>About Me</button>
+          </a>
+          <a href="#projects">
+            <button>Projects</button>
+          </a>
+          <a href="#contact">
+            <button>Contact</button>
+          </a>
+        </nav>
+      </div>
+    );
+  }
 }
